89a86676cf7b1e738a7e3af40a7443503afee6ee
Zachary Bennett <zbennett10@hotmail.com>
2017-07-30 14:18:53 -0700

    added redux promise middleware

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { Provider } from 'react-redux';
 import InspectApp from './containers';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 import AppReducer from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, thunkMiddleware)(createStore);
 const store = createStoreWithMiddleware(AppReducer);
 
 export default () => {
